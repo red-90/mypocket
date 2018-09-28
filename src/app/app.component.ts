306e1cee8a9347f7b03834c09ed092be0020f702
Radouane ABK <radouane.achebak@visiativ.com>
2018-09-28 10:54:33 +0100

    Check if user is logged during the launching of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,29 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-//import { HomePage } from '../pages/home/home';
+import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { AuthProvider } from '../providers/auth/auth';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage: any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public authProvider: AuthProvider, ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.authProvider.checkAuthentication().then((res)=>{
+      console.log("res : " + res)
+      if (res === '') this.rootPage  = LoginPage;
+      else this.rootPage  = HomePage;
+    })
   }
 }
 
